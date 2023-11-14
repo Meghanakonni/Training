428e492e66a678eafa1a04b507bd89058fc2e7ff
Meghanakonni <144089848+Meghanakonni@users.noreply.github.com>
2023-11-14 16:35:34 +0530

    CodeFirst Approach

---
+++ Tests/MVC/MVC_2/MVC_2/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC_2
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "MoviesByYear",
+                url: "{Movies}/{MoviesByYear}/{id}",
+                defaults: new { controller = "Movies", action = "MoviesByYear", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
